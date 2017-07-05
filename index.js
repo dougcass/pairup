13a9d5dbd0cbe0700b1f831a98f05f5dfa4a7388
Douglas Casserleigh <casserleigh@gmail.com>
2017-07-05 13:04:40 -0600

    added /home

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+//Landing Page
 app.get('/', function (req, res) {
     res.render("landing.ejs");
+});
+
+//User Home Page
+app.get('/home', function (req, res) {
+    res.render("home.ejs");
+});
+
+//Time_slots
+
+
+//Create Slot Page
+app.get('/new', function (req, res) {
+    res.render("home.ejs");
 });
 
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
 });
+
+
+//install nodemon -  npm -g nodemon
+//install body-parser
